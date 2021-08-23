f3e8cd1dd7823c0bd4e613ae5610ce58844dd3a2
Maria Arias de Reyna <ariasdereyna@redhat.com>
2021-08-23 14:07:02 +0200

    test(ViewDefinitionParserService): Improve coverage

--- api/src/test/java/io/zimara/backend/api/service/parser/viewdefinition/ViewDefinitionParserServiceTest.java
+++ api/src/test/java/io/zimara/backend/api/service/parser/viewdefinition/ViewDefinitionParserServiceTest.java
@@ -4,13 +4,16 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.zimara.backend.api.metadata.catalog.ViewDefinitionCatalog;
 import io.zimara.backend.model.step.Step;
 import io.zimara.backend.model.step.kamelet.KameletStep;
+import io.zimara.backend.model.view.ConstraintOperation;
 import io.zimara.backend.model.view.ViewDefinition;
+import io.zimara.backend.model.view.ViewDefinitionConstraint;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -47,5 +50,95 @@ class ViewDefinitionParserServiceTest {
         steps.add(new KameletStep());
         views = viewDefinitionParserService.parse(steps);
         Assertions.assertEquals(2, views.size());
+    }
+
+    @Test
+    void appliesToSize() {
+        List<Step> steps = new LinkedList<>();
+        ViewDefinition view = new ViewDefinition();
+        view.setConstraints(new ArrayList<>());
+        ViewDefinitionConstraint viewDefinitionConstraint = new ViewDefinitionConstraint();
+        viewDefinitionConstraint.setMandatory(false);
+        viewDefinitionConstraint.setOperation(ConstraintOperation.SIZE_GREATER_THAN);
+        viewDefinitionConstraint.setParameter("0");
+        view.getConstraints().add(viewDefinitionConstraint);
+        Assertions.assertFalse(viewDefinitionParserService.appliesTo(steps, view));
+
+        viewDefinitionConstraint = new ViewDefinitionConstraint();
+        viewDefinitionConstraint.setMandatory(false);
+        viewDefinitionConstraint.setOperation(ConstraintOperation.SIZE_EQUALS);
+        viewDefinitionConstraint.setParameter("0");
+        view.getConstraints().add(viewDefinitionConstraint);
+        Assertions.assertTrue(viewDefinitionParserService.appliesTo(steps, view));
+
+        view = new ViewDefinition();
+        view.setConstraints(new ArrayList<>());
+        viewDefinitionConstraint = new ViewDefinitionConstraint();
+        viewDefinitionConstraint.setMandatory(false);
+        viewDefinitionConstraint.setOperation(ConstraintOperation.SIZE_SMALLER_THAN);
+        viewDefinitionConstraint.setParameter("1");
+        view.getConstraints().add(viewDefinitionConstraint);
+        Assertions.assertTrue(viewDefinitionParserService.appliesTo(steps, view));
+    }
+
+    @Test
+    void appliesToConnector() {
+
+        final String stepName = "test-name";
+        final String stepId = "test-id";
+
+        List<Step> steps = new LinkedList<>();
+        KameletStep step = new KameletStep();
+        step.setName(stepName);
+        step.setId(stepId);
+        steps.add(step);
+
+        ViewDefinition view = new ViewDefinition();
+        view.setConstraints(new ArrayList<>());
+        ViewDefinitionConstraint viewDefinitionConstraint = new ViewDefinitionConstraint();
+        viewDefinitionConstraint.setMandatory(false);
+        viewDefinitionConstraint.setOperation(ConstraintOperation.CONTAINS_STEP_TYPE);
+        viewDefinitionConstraint.setParameter("no-type");
+        view.getConstraints().add(viewDefinitionConstraint);
+        Assertions.assertFalse(viewDefinitionParserService.appliesTo(steps, view));
+
+        viewDefinitionConstraint = new ViewDefinitionConstraint();
+        viewDefinitionConstraint.setMandatory(false);
+        viewDefinitionConstraint.setOperation(ConstraintOperation.CONTAINS_STEP_TYPE);
+        viewDefinitionConstraint.setParameter("CONNECTOR");
+        view.getConstraints().add(viewDefinitionConstraint);
+        Assertions.assertTrue(viewDefinitionParserService.appliesTo(steps, view));
+        
+        view = new ViewDefinition();
+        view.setConstraints(new ArrayList<>());
+        viewDefinitionConstraint = new ViewDefinitionConstraint();
+        viewDefinitionConstraint.setMandatory(false);
+        viewDefinitionConstraint.setOperation(ConstraintOperation.CONTAINS_STEP_NAME);
+        viewDefinitionConstraint.setParameter("no-name");
+        view.getConstraints().add(viewDefinitionConstraint);
+        Assertions.assertFalse(viewDefinitionParserService.appliesTo(steps, view));
+
+        viewDefinitionConstraint = new ViewDefinitionConstraint();
+        viewDefinitionConstraint.setMandatory(false);
+        viewDefinitionConstraint.setOperation(ConstraintOperation.CONTAINS_STEP_NAME);
+        viewDefinitionConstraint.setParameter(stepName);
+        view.getConstraints().add(viewDefinitionConstraint);
+        Assertions.assertTrue(viewDefinitionParserService.appliesTo(steps, view));
+
+        view = new ViewDefinition();
+        view.setConstraints(new ArrayList<>());
+        viewDefinitionConstraint = new ViewDefinitionConstraint();
+        viewDefinitionConstraint.setMandatory(false);
+        viewDefinitionConstraint.setOperation(ConstraintOperation.CONTAINS_STEP_IDENTIFIER);
+        viewDefinitionConstraint.setParameter("no-id");
+        view.getConstraints().add(viewDefinitionConstraint);
+        Assertions.assertFalse(viewDefinitionParserService.appliesTo(steps, view));
+
+        viewDefinitionConstraint = new ViewDefinitionConstraint();
+        viewDefinitionConstraint.setMandatory(false);
+        viewDefinitionConstraint.setOperation(ConstraintOperation.CONTAINS_STEP_IDENTIFIER);
+        viewDefinitionConstraint.setParameter(stepId);
+        view.getConstraints().add(viewDefinitionConstraint);
+        Assertions.assertTrue(viewDefinitionParserService.appliesTo(steps, view));
     }
 }
