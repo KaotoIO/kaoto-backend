3b666babc40b0c0def13d91d64cbd3e7095bdd64
Maria Arias de Reyna <ariasdereyna@redhat.com>
2022-02-10 10:42:29 +0100

    Adding new endpoint View Definitions

--- api/src/test/java/io/kaoto/backend/api/resource/ViewDefinitionResourceTest.java
+++ api/src/test/java/io/kaoto/backend/api/resource/ViewDefinitionResourceTest.java
@@ -1,5 +1,8 @@
 package io.kaoto.backend.api.resource;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.kaoto.backend.model.step.Step;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.kaoto.backend.api.metadata.catalog.StepCatalog;
@@ -9,11 +12,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.LinkedList;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
@@ -52,7 +58,7 @@ class ViewDefinitionResourceTest {
     }
 
     @Test
-    void testViews() {
+    void testViews() throws JsonProcessingException {
         var res = given()
                 .when().body(binding)
                 .contentType("text/yaml").post()
@@ -61,6 +67,24 @@ class ViewDefinitionResourceTest {
         res.body("views.size()", is(4),
                 "views[0].type", is("generic"),
                 "steps.size()", is(4));
+
+        List<Step> steps = new LinkedList<Step>();
+        steps.add(stepCatalog.getReadOnlyCatalog().searchStepByID(
+                                "kamelet:source"));
+        steps.add(stepCatalog.getReadOnlyCatalog().searchStepByID(
+                                "log"));
+
+        final var mapper = new ObjectMapper();
+        final var json = mapper.writeValueAsString(steps);
+        res = given()
+                .when().body(json)
+                .contentType(MediaType.APPLICATION_JSON).post()
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
+        res.body("views.size()", is(1),
+                "views[0].type", is("generic"),
+                "steps.size()", is(2));
+
     }
 
     @Test
