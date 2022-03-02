9078c35fea4f9c254c74454d27c1bf39e8f61429
Maria Arias de Reyna <ariasdereyna@redhat.com>
2022-03-02 13:03:39 +0100

    Removing useless code

--- kamelet-support/src/main/java/io/kaoto/backend/model/deployment/kamelet/step/choice/Choice.java
+++ kamelet-support/src/main/java/io/kaoto/backend/model/deployment/kamelet/step/choice/Choice.java
@@ -10,9 +10,6 @@ import java.util.List;
 @JsonPropertyOrder({"simple", "steps"})
 public class Choice {
 
-//    @JsonProperty("jsonpath")
-//    private JSONPath jsonPath;
-
     @JsonProperty("simple")
     private String simple;
 
@@ -27,15 +24,6 @@ public class Choice {
             final List<FlowStep> steps) {
         this.steps = steps;
     }
-//
-//    public JSONPath getJsonPath() {
-//        return jsonPath;
-//    }
-//
-//    public void setJsonPath(
-//            final JSONPath jsonPath) {
-//        this.jsonPath = jsonPath;
-//    }
 
     public String getSimple() {
         return simple;
