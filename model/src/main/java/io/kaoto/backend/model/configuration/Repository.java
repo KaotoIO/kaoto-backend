457a78c9671ac28ac1864b8ef40ebec14eacc5ce
Maria Arias de Reyna <ariasdereyna@redhat.com>
2022-07-05 14:14:44 +0200

    Using a better kebap naming for properties

--- model/src/main/java/io/kaoto/backend/model/configuration/Repository.java
+++ model/src/main/java/io/kaoto/backend/model/configuration/Repository.java
@@ -1,27 +1,36 @@
 package io.kaoto.backend.model.configuration;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import io.quarkus.runtime.annotations.StaticInitSafe;
 import io.smallrye.config.WithDefault;
+import io.smallrye.config.WithName;
 
 import java.util.List;
 import java.util.Optional;
 
+@StaticInitSafe
+@RegisterForReflection
 public interface Repository {
     Optional<List<Location>> jar();
     Optional<List<Git>> git();
     Optional<List<Location>> localFolder();
 
+    @RegisterForReflection
     interface Git {
         String url();
         @WithDefault("main")
         String tag();
         @WithDefault("false")
-        Boolean ifNoCluster();
+        @WithName("if-no-cluster")
+        boolean ifNoCluster();
     }
 
+    @RegisterForReflection
     interface Location {
         String url();
         @WithDefault("false")
-        Boolean whennocluster();
+        @WithName("if-no-cluster")
+        boolean ifNoCluster();
     }
 
 }
