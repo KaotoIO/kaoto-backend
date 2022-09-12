215c78290e2c5f44b08aa371dd406024421ec504
JoshiRaez <jraez@redhat.com>
2022-09-12 10:42:14 +0200

    Fixed compiler and checkstyle problems

--- kamelet-support/src/main/java/io/kaoto/backend/metadata/parser/step/camelRoute/CamelRouteFileProcessor.java
+++ kamelet-support/src/main/java/io/kaoto/backend/metadata/parser/step/camelRoute/CamelRouteFileProcessor.java
@@ -1,7 +1,11 @@
 package io.kaoto.backend.metadata.parser.step.camelRoute;
 
 import io.kaoto.backend.metadata.parser.JsonProcessFile;
-import io.kaoto.backend.model.parameter.*;
+import io.kaoto.backend.model.parameter.BooleanParameter;
+import io.kaoto.backend.model.parameter.ObjectParameter;
+import io.kaoto.backend.model.parameter.NumberParameter;
+import io.kaoto.backend.model.parameter.StringParameter;
+import io.kaoto.backend.model.parameter.Parameter;
 import io.kaoto.backend.model.step.Step;
 import org.jboss.logging.Logger;
 
@@ -12,9 +16,6 @@ import javax.json.JsonReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,27 +34,27 @@ public class CamelRouteFileProcessor extends JsonProcessFile<Step> {
         String type,
         List<Parameter> parameters,
         List<String> required
-    ) {}
+    ) { }
     private final Logger log = Logger.getLogger(CamelRouteFileProcessor.class);
 
     @Override
-    protected Step parseFile(File f) {
+    protected Step parseFile(final File f) {
         try (FileReader fr = new FileReader(f)) {
             JsonReader reader = Json.createReader(fr);
             JsonObject json = reader.readObject();
             Step step = convertToStep(json);
             return step;
         } catch (IOException | JsonException e) {
-            log.error("Error parsing '" + f.getAbsolutePath() + "'", e);
+            log.error("Error parsing '"  + f.getAbsolutePath() + "'", e);
         }
 
         return null;
     }
 
-    private Step convertToStep(JsonObject json) {
-        Step step = new Step();
-
+    private Step convertToStep(final JsonObject json) {
         JsonCamelObject parsedCamelJson = getDataFromJson(json);
+
+        Step step = new Step();
 
         step.setKind(parsedCamelJson.kind());
         step.setTitle(parsedCamelJson.title());
@@ -68,23 +69,10 @@ public class CamelRouteFileProcessor extends JsonProcessFile<Step> {
         return step;
     }
 
-    private JsonCamelObject getDataFromJson(JsonObject json) {
-        final String CAMEL_KIND = "Camel-Connector";
-        final String DEFAULT_GROUP = "Camel-Component";
-        final String DEFAULT_ICON = "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iaXNvLTg4NTktMSI" +
-            "/Pg0KPCEtLSBHZW5lcmF0b3I6IEFkb2JlIElsbHVzdHJhdG9yIDE2LjAuMCwgU1ZHIEV4cG9ydCBQbHVnLUluIC4gU1ZHIFZlcnNpb2" +
-            "46IDYuMDAgQnVpbGQgMCkgIC0tPg0KPCFET0NUWVBFIHN2ZyBQVUJMSUMgIi0vL1czQy8vRFREIFNWRyAxLjEvL0VOIiAiaHR0cDov" +
-            "L3d3dy53My5vcmcvR3JhcGhpY3MvU1ZHLzEuMS9EVEQvc3ZnMTEuZHRkIj4NCjxzdmcgdmVyc2lvbj0iMS4xIiBpZD0iQ2FwYV8xIiB" +
-            "4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIi" +
-            "B4PSIwcHgiIHk9IjBweCINCgkgd2lkdGg9IjUxMnB4IiBoZWlnaHQ9IjUxMnB4IiB2aWV3Qm94PSIwIDAgNTEyIDUxMiIgc3R5bGU9I" +
-            "mVuYWJsZS1iYWNrZ3JvdW5kOm5ldyAwIDAgNTEyIDUxMjsiIHhtbDpzcGFjZT0icHJlc2VydmUiPg0KPGc+DQoJPHBhdGggZD0iTTQ0" +
-            "OCwwSDY0QzQ2LjMyOCwwLDMyLDE0LjMxMywzMiwzMnY0NDhjMCwxNy42ODgsMTQuMzI4LDMyLDMyLDMyaDM4NGMxNy42ODgsMCwzMi0" +
-            "xNC4zMTIsMzItMzJWMzINCgkJQzQ4MCwxNC4zMTMsNDY1LjY4OCwwLDQ0OCwweiBNNjQsNDgwVjEyOGg4MHY2NEg5NnYxNmg0OHY0O" +
-            "Eg5NnYxNmg0OHY0OEg5NnYxNmg0OHY0OEg5NnYxNmg0OHY4MEg2NHogTTQ0OCw0ODBIMTYwdi04MGgyNTZ2LTE2DQoJCUgxNjB2LTQ4" +
-            "aDI1NnYtMTZIMTYwdi00OGgyNTZ2LTE2SDE2MHYtNDhoMjU2di0xNkgxNjB2LTY0aDI4OFY0ODB6Ii8+DQo8L2c+DQo8Zz4NCjwvZz4" +
-            "NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg" +
-            "0KPGc+DQo8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+D" +
-            "Qo8L3N2Zz4NCg==";
+    private JsonCamelObject getDataFromJson(final JsonObject json) {
+        final String camelKind = "Camel-Connector";
+        final String defaultGroup = "Camel-Component";
+        final String defaultIcon = defaultIconString;
 
         JsonObject component = json.getJsonObject("component");
         JsonObject properties = json.getJsonObject("properties");
@@ -93,21 +81,26 @@ public class CamelRouteFileProcessor extends JsonProcessFile<Step> {
         String description = component.getString("description");
         String type = getStepType(component);
 
-        Map<String, Map<String, String>> propertiesParsed = properties.entrySet().stream()
+        Map<String, Map<String, String>> propertiesParsed = properties
+                .entrySet().stream()
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         property -> {
                             var hashMap = new HashMap<String, String>();
                             var jsonObject = property.getValue().asJsonObject();
 
-                            jsonObject.forEach((key, value) -> hashMap.put(key, value.toString()));
+                            jsonObject.forEach((key, value) ->
+                                    hashMap.put(key, value.toString()));
 
                             return hashMap;
                         }
                 ));
 
-        List<String> requiredProperties = propertiesParsed.entrySet().stream().filter(
-                property -> Boolean.parseBoolean(property.getValue().getOrDefault("required", "false"))
+        List<String> requiredProperties = propertiesParsed
+                .entrySet().stream().filter(
+                    property -> Boolean.parseBoolean(
+                            property.getValue().getOrDefault(
+                                    "required", "false"))
         ).map(
                 Map.Entry::getKey
         ).toList();
@@ -117,38 +110,42 @@ public class CamelRouteFileProcessor extends JsonProcessFile<Step> {
                 parameter -> {
                     Map<String, String> parameterData = parameter.getValue();
 
-                    Map<String, Function<Map.Entry<String, Map<String,String>>, Parameter>> typeToClassConversion = Map.of(
+                    Map<String, Function<Map.Entry<
+                            String, Map<String, String>>,
+                            Parameter>> typeToClassConversion = Map.of(
                             "string", this::getStringParameter,
                             "object", this::getObjectParameter,
                             "integer", this::getNumberParameter,
                             "boolean", this::getBooleanParameter
                     );
 
-                    Parameter parsedParameter = typeToClassConversion.get(parameterData.get("type")).apply(parameter);
+                    Parameter parsedParameter = typeToClassConversion
+                            .get(parameterData.get("type")).apply(parameter);
 
                     return parsedParameter;
                 }
         ).toList();
 
         return new JsonCamelObject(
-                CAMEL_KIND,
-                DEFAULT_ICON,
+                camelKind,
+                defaultIcon,
                 title,
                 description,
-                DEFAULT_GROUP,
+                defaultGroup,
                 type,
                 parameters,
                 requiredProperties
         );
     }
 
-    private StringParameter getStringParameter(Map.Entry<String, Map<String,String>> parameter) {
+    private StringParameter getStringParameter(
+            final Map.Entry<String, Map<String, String>> parameter) {
         String id = parameter.getKey();
         Map<String, String> parameterData = parameter.getValue();
 
         StringParameter parsedParameter = new StringParameter(
                 id,
-                parameterData.get("displayName"), //I can put Id here too like in Yaml
+                parameterData.get("displayName"),
                 parameterData.get("description"),
                 parameterData.getOrDefault("defaultValue", null),
                 null
@@ -157,7 +154,8 @@ public class CamelRouteFileProcessor extends JsonProcessFile<Step> {
         return parsedParameter;
     }
 
-    private NumberParameter getNumberParameter(Map.Entry<String, Map<String,String>> parameter) {
+    private NumberParameter getNumberParameter(
+            final Map.Entry<String, Map<String, String>> parameter) {
         //IntegerParameter doesn't fit some long types there are present
         String id = parameter.getKey();
         Map<String, String> parameterData = parameter.getValue();
@@ -166,13 +164,15 @@ public class CamelRouteFileProcessor extends JsonProcessFile<Step> {
                 id,
                 parameterData.get("displayName"),
                 parameterData.get("description"),
-                Long.parseLong(parameterData.getOrDefault("defaultValue", "0"))
+                Long.parseLong(parameterData
+                        .getOrDefault("defaultValue", "0"))
         );
 
         return parsedParameter;
     }
 
-    private ObjectParameter getObjectParameter(Map.Entry<String, Map<String,String>> parameter) {
+    private ObjectParameter getObjectParameter(
+            final Map.Entry<String, Map<String, String>> parameter) {
         String id = parameter.getKey();
         Map<String, String> parameterData = parameter.getValue();
 
@@ -186,7 +186,8 @@ public class CamelRouteFileProcessor extends JsonProcessFile<Step> {
         return parsedParameter;
     }
 
-    private BooleanParameter getBooleanParameter(Map.Entry<String, Map<String,String>> parameter) {
+    private BooleanParameter getBooleanParameter(
+            final Map.Entry<String, Map<String, String>> parameter) {
         String id = parameter.getKey();
         Map<String, String> parameterData = parameter.getValue();
 
@@ -194,16 +195,17 @@ public class CamelRouteFileProcessor extends JsonProcessFile<Step> {
                 id,
                 parameterData.get("displayName"),
                 parameterData.get("description"),
-                Boolean.parseBoolean(parameterData.getOrDefault("defaultValue", "false"))
+                Boolean.parseBoolean(parameterData
+                        .getOrDefault("defaultValue", "false"))
         );
 
         return parsedParameter;
     }
 
-    private String getStepType(JsonObject component) {
-        final String SOURCE = "source";
-        final String SINK = "sink";
-        final String ACTION = "action";
+    private String getStepType(final JsonObject component) {
+        final String sourceType = "source";
+        final String sinkType = "sink";
+        final String actionType = "action";
 
         final boolean canBeSource = component.getBoolean("producerOnly");
         final boolean canBeSink = component.getBoolean("consumerOnly");
@@ -212,8 +214,38 @@ public class CamelRouteFileProcessor extends JsonProcessFile<Step> {
         //Or else, it has to be one of source or sink depending
         //Take care that this later has to be duplicated because
         //of the differences in how Camel components and kaoto components are
-        return canBeSource == canBeSink ? ACTION
-                : canBeSource ? SOURCE
-                : SINK;
+        return canBeSource == canBeSink ? actionType
+                : canBeSource ? sourceType
+                : sinkType;
     }
+
+    // Putting it at the end because is just a big string being
+    // used as placeholder
+    private final String defaultIconString = "data:image/svg+xml;base64"
+            + ",PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iaXNvLTg4N"
+            + "TktMSI/Pg0KPCEtLSBHZW5lcmF0b3I6IEFkb2JlIElsbHVzdHJ"
+            + "hdG9yIDE2LjAuMCwgU1ZHIEV4cG9ydCBQbHVnLUluIC4gU1ZHIF"
+            + "ZlcnNpb246IDYuMDAgQnVpbGQgMCkgIC0tPg0KPCFET0NUWVBF"
+            + "IHN2ZyBQVUJMSUMgIi0vL1czQy8vRFREIFNWRyAxLjEvL0VOIi"
+            + "AiaHR0cDovL3d3dy53My5vcmcvR3JhcGhpY3MvU1ZHLzEuMS9E"
+            + "VEQvc3ZnMTEuZHRkIj4NCjxzdmcgdmVyc2lvbj0iMS4xIiBpZD"
+            + "0iQ2FwYV8xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAw"
+            + "MC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy"
+            + "8xOTk5L3hsaW5rIiB4PSIwcHgiIHk9IjBweCINCgkgd2lkdGg9"
+            + "IjUxMnB4IiBoZWlnaHQ9IjUxMnB4IiB2aWV3Qm94PSIwIDAgNT"
+            + "EyIDUxMiIgc3R5bGU9ImVuYWJsZS1iYWNrZ3JvdW5kOm5ldyAw"
+            + "IDAgNTEyIDUxMjsiIHhtbDpzcGFjZT0icHJlc2VydmUiPg0KPG"
+            + "c+DQoJPHBhdGggZD0iTTQ0OCwwSDY0QzQ2LjMyOCwwLDMyLDE0"
+            + "LjMxMywzMiwzMnY0NDhjMCwxNy42ODgsMTQuMzI4LDMyLDMyLD"
+            + "MyaDM4NGMxNy42ODgsMCwzMi0xNC4zMTIsMzItMzJWMzINCgkJ"
+            + "QzQ4MCwxNC4zMTMsNDY1LjY4OCwwLDQ0OCwweiBNNjQsNDgwVj"
+            + "EyOGg4MHY2NEg5NnYxNmg0OHY0OEg5NnYxNmg0OHY0OEg5NnYx"
+            + "Nmg0OHY0OEg5NnYxNmg0OHY4MEg2NHogTTQ0OCw0ODBIMTYwdi"
+            + "04MGgyNTZ2LTE2DQoJCUgxNjB2LTQ4aDI1NnYtMTZIMTYwdi00"
+            + "OGgyNTZ2LTE2SDE2MHYtNDhoMjU2di0xNkgxNjB2LTY0aDI4OF"
+            + "Y0ODB6Ii8+DQo8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0K"
+            + "PGc+DQo8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQ"
+            + "o8L2c+DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+"
+            + "DQo8Zz4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8Zz"
+            + "4NCjwvZz4NCjxnPg0KPC9nPg0KPGc+DQo8L2c+DQo8L3N2Zz4NCg==";
 }
--- kamelet-support/src/main/java/io/kaoto/backend/metadata/parser/step/camelRoute/CamelRouteParseCatalog.java
+++ kamelet-support/src/main/java/io/kaoto/backend/metadata/parser/step/camelRoute/CamelRouteParseCatalog.java
@@ -1,17 +1,28 @@
 package io.kaoto.backend.metadata.parser.step.camelRoute;
 
+import io.fabric8.kubernetes.client.KubernetesClient;
 import io.kaoto.backend.api.metadata.catalog.StepCatalogParser;
 import io.kaoto.backend.metadata.ParseCatalog;
+import io.kaoto.backend.metadata.parser.ClusterParseCatalog;
 import io.kaoto.backend.metadata.parser.GitParseCatalog;
 import io.kaoto.backend.metadata.parser.JarParseCatalog;
+import io.kaoto.backend.metadata.parser.LocalFolderParseCatalog;
+import io.kaoto.backend.model.deployment.kamelet.Kamelet;
 import io.kaoto.backend.model.step.Step;
 
 import javax.enterprise.context.ApplicationScoped;
-import java.nio.file.FileVisitor;
+import javax.inject.Inject;
 import java.nio.file.Path;
 
 @ApplicationScoped
 public final class CamelRouteParseCatalog implements StepCatalogParser {
+
+    @Inject
+    public void setKubernetesClient(final KubernetesClient kubernetesClient) {
+        this.kubernetesClient = kubernetesClient;
+    }
+    private KubernetesClient kubernetesClient;
+
     private CamelRouteParseCatalog() {
 
     }
@@ -28,6 +39,23 @@ public final class CamelRouteParseCatalog implements StepCatalogParser {
     public ParseCatalog<Step> getParser(final String url) {
 
         ParseCatalog<Step> parseCatalog = new JarParseCatalog<>(url);
+        parseCatalog.setFileVisitor(new CamelRouteFileProcessor());
+        return parseCatalog;
+    }
+
+    @Override
+    public ParseCatalog<Step> getParserFromCluster() {
+        ClusterParseCatalog<Step> parseCatalog =
+                new ClusterParseCatalog<>(Kamelet.class);
+        parseCatalog.setFileVisitor(new CamelRouteFileProcessor());
+        parseCatalog.setKubernetesClient(kubernetesClient);
+        return parseCatalog;
+    }
+
+    @Override
+    public ParseCatalog<Step> getLocalFolder(final Path path) {
+        ParseCatalog<Step> parseCatalog =
+                new LocalFolderParseCatalog<>(path);
         parseCatalog.setFileVisitor(new CamelRouteFileProcessor());
         return parseCatalog;
     }
