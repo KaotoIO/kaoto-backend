ad044da911a9d4feff8f7b86b8dc8c4f90d5f0f3
Maria Arias de Reyna <ariasdereyna@redhat.com>
2022-09-26 12:39:47 +0200

    feature(steps): Add pagination to catalog calls

--- api/src/test/java/io/kaoto/backend/api/resource/v1/StepResourceTest.java
+++ api/src/test/java/io/kaoto/backend/api/resource/v1/StepResourceTest.java
@@ -7,9 +7,11 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import javax.inject.Inject;
 import java.util.Collection;
+import java.util.List;
 
 @QuarkusTest
 @TestHTTPEndpoint(StepResource.class)
@@ -52,8 +54,44 @@ class StepResourceTest {
     }
 
     @Test
+    @Timeout(100)
+    void speedKamelet() {
+        stepResource.all("KameletBinding", null, null, null, null);
+    }
+
+    @Test
+    @Timeout(100)
+    void speedKameletAndBinding() {
+        stepResource.all("KameletBinding,Kamelet", null, null, null, null);
+    }
+    @Test
+    @Timeout(100)
+    void speedKameletAndBindingEnd() {
+        stepResource.all("KameletBinding,Kamelet", "END", null, null, null);
+    }
+    @Test
+    @Timeout(100)
+    void speedKameletBindingMiddle() {
+        stepResource.all("KameletBinding", "MIDDLE", null, null, null);
+    }
+
+    @Test
+    void limitAndStart() {
+        Assertions.assertTrue(stepResource.all(null, null, null, null, null).size() > 10);
+        List<Step> limit = stepResource.all(null, null, null, 10l, null).stream().toList();
+        Assertions.assertEquals(10, limit.size());
+
+        List<Step> start = stepResource.all(null, null, null, 10l, 5l).stream().toList();
+        Assertions.assertEquals(10, start.size());
+
+        for (int i = 0; i <= 5; i++) {
+            Assertions.assertEquals(limit.get(5 + i), start.get(i));
+        }
+    }
+
+    @Test
     void allSteps() {
-        Collection<Step> steps = stepResource.all(null, null, null);
+        Collection<Step> steps = stepResource.all(null, null, null, null, null);
         Assertions.assertNotNull(steps);
         Assertions.assertEquals(
                 catalog.getReadOnlyCatalog().getAll().size(),
@@ -62,26 +100,26 @@ class StepResourceTest {
         var integrationType = "KameletBinding";
         Assertions.assertNotNull(steps);
         Assertions.assertTrue(
-                stepResource.all(null, integrationType, null)
+                stepResource.all(null, integrationType, null, null, null)
                         .stream().allMatch(s ->
                                 integrationType.equalsIgnoreCase(s.getType())));
 
         var kind = "Kamelet";
         Assertions.assertNotNull(steps);
         Assertions.assertTrue(
-                stepResource.all(null, null, kind)
+                stepResource.all(null, null, kind, null, null)
                         .stream().allMatch(s ->
                                 kind.equalsIgnoreCase(s.getKind())));
         var type = "START";
         Assertions.assertNotNull(steps);
         Assertions.assertTrue(
-                stepResource.all(type, null, null)
+                stepResource.all(type, null, null, null, null)
                         .stream().allMatch(s ->
                                 type.equalsIgnoreCase(s.getType())));
 
         Assertions.assertNotNull(steps);
         Assertions.assertTrue(
-                stepResource.all(type, integrationType, kind)
+                stepResource.all(type, integrationType, kind, null, null)
                         .stream().allMatch(s ->
                                 type.equalsIgnoreCase(s.getType())
                                 && kind.equalsIgnoreCase(s.getKind())));
