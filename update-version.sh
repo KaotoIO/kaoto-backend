fc4348e5ad08df16e32fe61f3b527184336d7086
Maria Arias de Reyna <ariasdereyna@redhat.com>
2022-11-16 10:57:48 +0100

    feature(release): Downloading zip files automatically

--- update-version.sh
+++ update-version.sh
@@ -3,7 +3,42 @@
 git reset --hard 
 git pull
 
-echo "Enter new version:"
+echo "Do you want to download the latest zips of steps? (y/n)"
+echo "Note: If you want to change the version of Apache Camel, "
+echo "you have to generate it independently from this script."
+read -r download
+
+if [ "$download" = "y" ];
+then
+
+  echo "Downloading camel component metadata."
+  wget https://github.com/KaotoIO/camel-component-metadata/archive/refs/heads/main.zip
+  mv "main.zip" "api/src/main/resources/camel-component-metadata.zip"
+  git add api/src/main/resources/camel-component-metadata.zip
+
+  echo "Downloading kamelets."
+  echo "Enter kamelets version:"
+  echo "Example: 0.9.5"
+  read -r versionKamelets
+
+  path=$(pwd)
+  cd /tmp
+  mkdir kamelets
+  cd kamelets
+  wget "https://github.com/apache/camel-kamelets/archive/refs/tags/v"$versionKamelets".zip"
+  unzip *zip
+  cd camel-kamelets-0.9.3/kamelets/
+  zip "camel-kamelets-"$versionKamelets".jar" *
+  mv "camel-kamelets-"$versionKamelets".jar" $path"/api/src/main/resources/"
+  cd $path
+  git add "api/src/main/resources/camel-kamelets-"$versionKamelets".jar"
+  sed -i 's/kamelets-.*/kamelets-'$versionKamelets'.jar$"/g' api/src/main/resources/resources-config.json
+  git add api/src/main/resources/resources-config.json
+
+  git commit -m "Updating zip files with step catalog components."
+fi
+
+echo "Enter version of Kaoto to release:"
 echo "Example: 1.0.0"
 read -r version
 
@@ -12,24 +47,25 @@ git add .
 git commit -m "Updating to version $version"
 git tag v$version
 
+echo "Now we are going to test the new version."
+
 mvn install
 
-echo "Did the tests pass? Do you want to push? (y/n)" 
-read -r shouldPush
+echo "Enter new development version:"
+echo "(SNAPSHOT will be added automatically)"
+echo "Example: 1.0.1"
+read -r version2
 
-if [ "$shouldPush" = "y" ];
-then
+mvn versions:set -DnewVersion="$version2"-SNAPSHOT
+git add .
+git commit -m "Updating to version $version2-SNAPSHOT"
 
-  echo "Enter new version:"
-  echo "Example: 1.0.1"
-  read -r version2
-
-  mvn versions:set -DnewVersion="$version2"-SNAPSHOT
-  git add .
-  git commit -m "Updating to version $version2-SNAPSHOT"
-  git push
-  
-  echo "Now go to https://github.com/KaotoIO/kaoto-backend/releases/ to review the text of the release and publish it. "
-  echo "Congratulations! Your job here is done."
-  
-fi
+echo ""
+echo "Check the git log and if you like what you see, just do"
+echo "'git push' and 'git push --tags'."
+echo "This will trigger the release creation."
+echo ""
+echo "Then go to https://github.com/KaotoIO/kaoto-backend/releases/ to review"
+echo "the text of the release and publish it. "
+echo ""
+echo "Congratulations! Your job here is done."
