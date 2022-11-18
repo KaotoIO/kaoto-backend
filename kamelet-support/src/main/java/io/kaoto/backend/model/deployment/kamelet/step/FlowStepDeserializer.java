2a3ae570d7725524dfecc33d621242da0d974d29
Maria Arias de Reyna <ariasdereyna@redhat.com>
2022-11-18 13:19:28 +0100

    feature(eip): Add Load Balance EIP

--- kamelet-support/src/main/java/io/kaoto/backend/model/deployment/kamelet/step/FlowStepDeserializer.java
+++ kamelet-support/src/main/java/io/kaoto/backend/model/deployment/kamelet/step/FlowStepDeserializer.java
@@ -1,10 +1,12 @@
 package io.kaoto.backend.model.deployment.kamelet.step;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.kaoto.backend.model.deployment.kamelet.FlowStep;
 import org.jboss.logging.Logger;
 
 import java.util.HashMap;
@@ -19,15 +21,19 @@ public class FlowStepDeserializer extends JsonDeserializer {
             final DeserializationContext deserializationContext) {
         try {
             JsonNode n = jsonParser.getCodec().readTree(jsonParser);
-
-            for (var step : getFlowSteps().entrySet()) {
-                if (n.get(step.getKey()) != null) {
-                    return new ObjectMapper().readValue(n.toPrettyString(), step.getValue());
-                }
-            }
-
+            return deserializeFlowStep(n);
         } catch (Exception e) {
             log.error("Error trying to deserialize step: " + e.getMessage());
+        }
+
+        return new UriFlowStep();
+    }
+
+    public FlowStep deserializeFlowStep(final JsonNode n) throws JsonProcessingException {
+        for (var step : getFlowSteps().entrySet()) {
+            if (n.get(step.getKey()) != null) {
+                return (FlowStep) new ObjectMapper().readValue(n.toPrettyString(), step.getValue());
+            }
         }
 
         return new UriFlowStep();
@@ -53,6 +59,8 @@ public class FlowStepDeserializer extends JsonDeserializer {
         steps.put("idempotent-consumer", IdempotentConsumerFlowStep.class);
         steps.put("idempotentConsumer", IdempotentConsumerFlowStep.class);
         steps.put("marshal", MarshalFlowStep.class);
+        steps.put("load-balance", LoadBalanceFlowStep.class);
+        steps.put("loadBalance", LoadBalanceFlowStep.class);
         steps.put("log", LogFlowStep.class);
         steps.put("loop", LoopFlowStep.class);
         steps.put("multicast", MulticastFlowStep.class);
