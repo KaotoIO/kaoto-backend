69ab1e31a6a288a449bdd48b43cb24883e87397d
Maria Arias de Reyna <ariasdereyna@redhat.com>
2022-11-23 11:18:33 +0100

    feature(speedup): Simplified kamelet classes for the step catalog warmup
    This reduces the startup time from an average (on my machine) of 2.5
    seconds to 1.5 seconds. We need to go deeper.

---
+++ kamelet-support/src/main/java/io/kaoto/backend/model/deployment/kamelet/SimplifiedKamelet.java
@@ -0,0 +1,57 @@
+package io.kaoto.backend.model.deployment.kamelet;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+
+
+/**
+ * 🐱class SimplifiedKamelet
+ *
+ * Used to read kamelet properties, but not digging into the implementation of the kamelet.
+ *
+ */
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonDeserialize(using = JsonDeserializer.None.class)
+public final class SimplifiedKamelet {
+
+    @JsonProperty("spec")
+    protected SimplifiedKameletSpec spec;
+
+    @JsonProperty("kind")
+    private String kind;
+
+    @JsonProperty("metadata")
+    private ObjectMeta metadata = new ObjectMeta();
+
+    public SimplifiedKamelet() {
+    }
+
+    public SimplifiedKameletSpec getSpec() {
+        return spec;
+    }
+
+    public void setSpec(final SimplifiedKameletSpec spec) {
+        this.spec = spec;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(final String kind) {
+        this.kind = kind;
+    }
+
+    public ObjectMeta getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(final ObjectMeta metadata) {
+        this.metadata = metadata;
+    }
+}
