1b99d39531ef8646af13a96b0c5c9b9fda8706fa
Maria Arias de Reyna <ariasdereyna@redhat.com>
2022-11-30 15:27:44 +0100

    codesmells: static fields, unused imports

--- kamelet-support/src/main/java/io/kaoto/backend/model/deployment/kamelet/step/SagaFlowStep.java
+++ kamelet-support/src/main/java/io/kaoto/backend/model/deployment/kamelet/step/SagaFlowStep.java
@@ -3,7 +3,6 @@ package io.kaoto.backend.model.deployment.kamelet.step;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.kaoto.backend.KamelPopulator;
