beeb1ec77479b8c67bfa9ecb3bf4c6dc5bfe2124
Maria Arias de Reyna <ariasdereyna@redhat.com>
2022-12-13 13:02:13 +0100

    test(stepResourceIT): Improved the tests to check all three core sources are included

--- api/src/test/java/io/kaoto/backend/api/resource/v1/StepResourceIT.java
+++ api/src/test/java/io/kaoto/backend/api/resource/v1/StepResourceIT.java
@@ -2,7 +2,10 @@ package io.kaoto.backend.api.resource.v1;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.kaoto.backend.api.metadata.catalog.StepCatalog;
+import io.kaoto.backend.api.service.language.LanguageService;
 import org.assertj.core.api.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,9 +15,10 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
 
+import javax.inject.Inject;
+
 @QuarkusIntegrationTest
 class StepResourceIT {
-
     @Test
     void checkCatalogLoaded() throws Exception {
         ValidatableResponse response = RestAssured.given().get("/v1/steps").then().statusCode(200);
@@ -23,13 +27,19 @@ class StepResourceIT {
         ObjectMapper objectMapper = new ObjectMapper();
         Step[] steps = objectMapper.readValue(json, Step[].class);
         assertThat(steps).isNotEmpty();
-        Condition<Step> conditionSourceKamelet = new Condition<>(value -> value.getName().contains("-source"),
-                "Step highly probable generated from source from kamelet catalog ");
-        Condition<Step> conditionSinkKamelet = new Condition<>(value -> value.getName().contains("-sink"),
-                "Step highly probable generated from sink from kamelet catalog ");
+        Condition<Step> weHaveCamelConnectors =
+                new Condition<>(value -> value.getKind().equalsIgnoreCase("Camel-Connector"),
+                "We have camel connectors");
+        Condition<Step> weHaveKamelets =
+                new Condition<>(value -> value.getKind().equalsIgnoreCase("Kamelet"),
+                        "We have kamelets.");
+        Condition<Step> weHaveEIPs =
+                new Condition<>(value -> value.getKind().equalsIgnoreCase("EIP"),
+                        "We have EIPs.");
         assertThat(steps)
-            .haveAtLeast(10, conditionSourceKamelet)
-            .haveAtLeast(10, conditionSinkKamelet);
+                .haveAtLeast(10, weHaveCamelConnectors)
+                .haveAtLeast(10, weHaveKamelets)
+                .haveAtLeast(10, weHaveEIPs);
     }
     
 }
