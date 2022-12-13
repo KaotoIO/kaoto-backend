de26442dacd8a2d66537a620c98db9d2502f08ce
Maria Arias de Reyna <ariasdereyna@redhat.com>
2022-12-13 10:48:36 +0100

    feature(release): Add camel components to update script

--- update-version.sh
+++ update-version.sh
@@ -4,21 +4,41 @@ git reset --hard
 git pull
 
 echo "Do you want to download the latest zips of steps? (y/n)"
-echo "Note: If you want to change the version of Apache Camel, "
-echo "you have to generate it independently from this script."
+echo "Note: This includes Apache Camel components, kamelets, and kaoto bridged camel components."
 read -r download
 
 if [ "$download" = "y" ];
 then
 
-  echo "Downloading camel component metadata."
+  echo "Downloading Kaoto bridged Camel components (EIP)."
   wget https://github.com/KaotoIO/camel-component-metadata/archive/refs/heads/main.zip
   mv "main.zip" "api/src/main/resources/camel-component-metadata.zip"
   git add -f "api/src/main/resources/camel-component-metadata.zip"
-
+  
+  echo "Downloading camel connectors."
+  echo "Enter camel version to use:"
+  echo "Example: 3.18.4"
+  read -r versionCamel
+  
+  path=$(pwd)
+  git rm $path"/api/src/main/resources/camel-*.zip"
+  cd /tmp
+  mkdir camel-connectors
+  cd camel-connectors
+  wget "https://github.com/apache/camel/archive/refs/tags/camel-"$versionCamel".zip"
+  unzip *zip
+  cd "camel-camel-"$versionCamel"/catalog/camel-catalog/src/generated/resources/org/apache/camel/catalog/components/"
+  zip "camel-connectors-"$versionCamel".zip" *.json
+  mv "camel-connectors-"$versionCamel".zip" $path"/api/src/main/resources/"
+  cd $path
+  git add -f "api/src/main/resources/camel-connectors-"$versionCamel".zip"
+  sed -i 's/camel-connectors-.*/camel-connectors-'$versionCamel'.zip$"/g' api/src/main/resources/resources-config.json
+  sed -i 's/camel-connectors-.*/camel-connectors-'$versionCamel'.zip"/g' api/src/main/resources/application.yaml
+  sed -i 's/camel-connectors-.*/camel-connectors-'$versionCamel'.zip"/g' api/src/test/resources/application.yaml
+  
   echo "Downloading kamelets."
-  echo "Enter kamelets version:"
-  echo "Example: 0.9.5"
+  echo "Enter kamelets version to use:"
+  echo "Example: 1.0.0"
   read -r versionKamelets
 
   path=$(pwd)
