3ce9c0bfa6fd40811d7cc9f79581e04072396eb3
Ricardo M <lordrip@gmail.com>
2023-02-10 11:27:50 +0100

    fix(steps): Details for some steps are not shown
    The UI requires that the 'required' property from Step to be
    of an array type.
    
    This commit adds a default value of an empty array for those
    steps thadoesn't have any required property.
    
    fixes [KaotoUI #1231](https://github.com/KaotoIO/kaoto-ui/issues/1231)

--- api/src/test/java/io/kaoto/backend/api/resource/v1/StepResourceIT.java
+++ api/src/test/java/io/kaoto/backend/api/resource/v1/StepResourceIT.java
@@ -1,16 +1,14 @@
 package io.kaoto.backend.api.resource.v1;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.assertj.core.api.Condition;
-import org.junit.jupiter.api.Test;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import io.kaoto.backend.model.step.Step;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
+import org.assertj.core.api.Condition;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @QuarkusIntegrationTest
 class StepResourceIT {
@@ -36,5 +34,16 @@ class StepResourceIT {
                 .haveAtLeast(10, weHaveKamelets)
                 .haveAtLeast(10, weHaveEIPs);
     }
-    
+
+    @Test
+    void checkAllRequiredPropertiesInitialized() throws Exception {
+        ValidatableResponse response = RestAssured.given().get("/v1/steps").then().statusCode(200);
+
+        String json = response.extract().body().asString();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Step[] steps = objectMapper.readValue(json, Step[].class);
+
+        assertThat(steps)
+                .allMatch(step -> step.getRequired() != null);
+    }
 }
