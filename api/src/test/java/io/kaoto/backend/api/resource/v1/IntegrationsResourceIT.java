1d601efc51bc394908e896cdb2124c984d8b490a
Aurélien Pupier <apupier@redhat.com>
2023-04-07 09:20:10 +0200

    Provide native tests for Integrations resource
    - it allows to have a non-regression test for #617 issue with
    expressions
    - extracted a single test from IntegrationsResourceTest which cannot be
    launch in Native mode because it is using @Inject to access internal
    services directly. it allows to inherit the native test class from
    IntegrationsResourceTest and play all of the existing tests in native
    
    part of #617
    part of #437
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

---
+++ api/src/test/java/io/kaoto/backend/api/resource/v1/IntegrationsResourceIT.java
@@ -0,0 +1,8 @@
+package io.kaoto.backend.api.resource.v1;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class IntegrationsResourceIT extends IntegrationsResourceTest {
+
+}
