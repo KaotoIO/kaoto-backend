b78fb0ed72a0757bd0689d1a2954dc5fdf0ac4cd
mkralik3 <mkralik@redhat.com>
2023-05-31 14:04:52 +0200

    test: Add more tests for Capabilities resource

--- api/src/test/java/io/kaoto/backend/api/resource/v1/CapabilitiesResourceIT.java
+++ api/src/test/java/io/kaoto/backend/api/resource/v1/CapabilitiesResourceIT.java
@@ -3,6 +3,6 @@ package io.kaoto.backend.api.resource.v1;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-class CapabilitiesResourceIT  extends CapabilitiesResourceTest{
+class CapabilitiesResourceIT extends CapabilitiesResourceTest{
 
 }
--- api/src/test/java/io/kaoto/backend/api/service/language/LanguageServiceTest.java
+++ api/src/test/java/io/kaoto/backend/api/service/language/LanguageServiceTest.java
@@ -1,10 +1,11 @@
 package io.kaoto.backend.api.service.language;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
+
+import static io.kaoto.backend.api.service.language.LanguagesSpecificationChecker.checkAllLanguagesSpecifications;
 
 
 @QuarkusTest
@@ -20,27 +21,6 @@ class LanguageServiceTest {
 
     @Test
     void getAll() {
-        var map = languageService.getAll();
-        Assertions.assertEquals(4, map.size());
-
-        for (var language : map) {
-            if (String.valueOf(language.get("name")).equalsIgnoreCase("KameletBinding")) {
-                Assertions.assertEquals("true", language.get("default"));
-                Assertions.assertEquals("true", language.get("input"));
-                Assertions.assertEquals("true", language.get("output"));
-                Assertions.assertEquals("true", language.get("deployable"));
-            } else if (String.valueOf(language.get("name")).equalsIgnoreCase("Kamelet")) {
-                Assertions.assertFalse(language.containsKey("default"));
-                Assertions.assertEquals("true", language.get("input"));
-                Assertions.assertEquals("true", language.get("output"));
-                Assertions.assertEquals("true", language.get("deployable"));
-            } else if (String.valueOf(language.get("name")).equalsIgnoreCase("Camel Route")) {
-                Assertions.assertFalse(language.containsKey("default"));
-                Assertions.assertEquals("true", language.get("input"));
-                Assertions.assertEquals("true", language.get("output"));
-                Assertions.assertEquals("false", language.get("deployable"));
-            }
-        }
+        checkAllLanguagesSpecifications(languageService.getAll());
     }
-
 }
