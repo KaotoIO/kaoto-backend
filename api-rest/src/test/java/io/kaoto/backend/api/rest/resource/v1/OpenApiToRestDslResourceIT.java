4c3606868fcb88b7a043a9312a7a0f971be3a2eb
Tomohisa Igarashi <tm.igarashi@gmail.com>
2023-06-22 18:13:07 -0400

    feat(restdsl): Add openapi-restdsl generator REST endpoint
    Fixes: #709

---
+++ api-rest/src/test/java/io/kaoto/backend/api/rest/resource/v1/OpenApiToRestDslResourceIT.java
@@ -0,0 +1,7 @@
+package io.kaoto.backend.api.rest.resource.v1;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class OpenApiToRestDslResourceIT extends OpenApiToRestDslResourceTest {
+}
---
+++ api-rest/src/test/java/io/kaoto/backend/api/rest/resource/v1/OpenApiToRestDslResourceTest.java
@@ -0,0 +1,129 @@
+package io.kaoto.backend.api.rest.resource.v1;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+import org.yaml.snakeyaml.LoaderOptions;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.SafeConstructor;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@QuarkusTest
+@TestHTTPEndpoint(OpenApiToRestDslResource.class)
+class OpenApiToRestDslResourceTest {
+
+    @Test
+    void jsonToYaml() throws Exception {
+        String json = Files.readString(Path.of(
+                OpenApiToRestDslResourceTest.class.getResource(
+                                "openapi-petstore.json")
+                        .toURI()));
+
+        var res = given()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(json)
+                .post("/")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
+
+        String answer = res.extract().body().asString();
+        verifyYaml(answer);
+    }
+
+    @Test
+    void jsonToXml() throws Exception {
+        String json = Files.readString(Path.of(
+                OpenApiToRestDslResourceTest.class.getResource(
+                                "openapi-petstore.json")
+                        .toURI()));
+
+        var res = given()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(json)
+                .post("/?output=xml")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
+
+        String answer = res.extract().body().asString();
+        verifyXml(answer);
+    }
+
+    @Test
+    void yamlToYaml() throws Exception {
+        String json = Files.readString(Path.of(
+                OpenApiToRestDslResourceTest.class.getResource(
+                                "openapi-petstore.yaml")
+                        .toURI()));
+
+        var res = given()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(json)
+                .post("/")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
+
+        String answer = res.extract().body().asString();
+        verifyYaml(answer);
+    }
+
+    @Test
+    void yamlToXml() throws Exception {
+        String json = Files.readString(Path.of(
+                OpenApiToRestDslResourceTest.class.getResource(
+                                "openapi-petstore.yaml")
+                        .toURI()));
+
+        var res = given()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(json)
+                .post("/?output=xml")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
+
+        String answer = res.extract().body().asString();
+        verifyXml(answer);
+    }
+
+    private void verifyYaml(String answer) throws Exception {
+        final ObjectMapper mapper = new ObjectMapper();
+        var loader = new Yaml(new SafeConstructor(new LoaderOptions()));
+        ArrayList list = loader.load(answer);
+        var actual = (ArrayNode) mapper.convertValue(list, JsonNode.class);
+        list = loader.load(Files.readString(Path.of(
+                OpenApiToRestDslResourceTest.class.getResource(
+                                "restdsl-petstore.yaml")
+                        .toURI())));
+        var expected = (ArrayNode) mapper.convertValue(list, JsonNode.class);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private void verifyXml(String answer) throws Exception {
+        var mapper = new XmlMapper();
+        var actual = (ObjectNode) mapper.readTree(answer);
+        var expected = (ObjectNode) mapper.readTree(Files.readString(Path.of(
+                OpenApiToRestDslResourceTest.class.getResource(
+                                "restdsl-petstore.xml")
+                        .toURI())));
+        assertThat(actual).isEqualTo(expected);
+    }
+
+}
