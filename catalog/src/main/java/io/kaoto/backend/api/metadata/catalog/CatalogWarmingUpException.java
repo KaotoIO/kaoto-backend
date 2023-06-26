d371e07ff5e7158cf9149829ab5577ced32ffb8a
Maria Arias de Reyna <ariasdereyna@redhat.com>
2023-06-26 13:07:27 +0200

    feature: Wait for the warmup instead of throwing exception

--- catalog/src/main/java/io/kaoto/backend/api/metadata/catalog/CatalogWarmingUpException.java
+++
@@ -1,8 +0,0 @@
-package io.kaoto.backend.api.metadata.catalog;
-
-public class CatalogWarmingUpException extends RuntimeException {
-
-    CatalogWarmingUpException(final String message) {
-        super(message);
-    }
-}
