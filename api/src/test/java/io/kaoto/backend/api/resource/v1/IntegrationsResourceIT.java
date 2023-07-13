9a48d4d2a0d4906e3e02163ec06d985a00eb4bca
mkralik3 <mkralik@redhat.com>
2023-07-13 17:36:17 +0200

    test: Use all IntegrationsResource tests in native testing
    Test from IntegrationsResourceWithServiceAccessTest was moved into IntegrationsResourceTestAbstract to be able to run in native testing. It was also broken into 3 separate tests.

--- api/src/test/java/io/kaoto/backend/api/resource/v1/IntegrationsResourceIT.java
+++ api/src/test/java/io/kaoto/backend/api/resource/v1/IntegrationsResourceIT.java
@@ -4,10 +4,20 @@ import io.kaoto.backend.api.metadata.catalog.StepCatalog;
 import io.kaoto.backend.api.resource.support.NativeHelper;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
+import java.util.List;
+
 @QuarkusIntegrationTest
 class IntegrationsResourceIT extends IntegrationsResourceTestAbstract {
     @Override
     protected void waitForWarmUpCatalog() {
         NativeHelper.waitForWarmUpCatalog(StepCatalog.class.getCanonicalName());
+    }
+
+    @Override
+    protected List<String> getAllLanguages() {
+        return List.of("KameletBinding",
+                "Kamelet",
+                "Camel Route",
+                "Integration");
     }
 }
