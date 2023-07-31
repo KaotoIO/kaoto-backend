9c2913ac93c4f0114db5aa2e621218e2f8806ee0
Maria Arias de Reyna <ariasdereyna@redhat.com>
2023-07-31 14:31:05 +0200

    refactor (jackson): Use directly jsonGenerator instead of a map

--- camel-support/src/main/java/io/kaoto/backend/camel/model/deployment/camelroute/IntegrationSpecSerializer.java
+++ camel-support/src/main/java/io/kaoto/backend/camel/model/deployment/camelroute/IntegrationSpecSerializer.java
@@ -5,9 +5,6 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 class IntegrationSpecSerializer extends StdSerializer<IntegrationSpec> {
 
@@ -19,20 +16,42 @@ class IntegrationSpecSerializer extends StdSerializer<IntegrationSpec> {
     public void serialize(IntegrationSpec data, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
             throws IOException {
 
-        Map<String, Object> properties = new LinkedHashMap<>();
-        properties.put("configuration", data.getConfiguration());
-        properties.put("dependencies", data.getDependencies());
-        properties.put("integrationKit", data.getIntegrationKit());
-        properties.put("profile", data.getProfile());
-        properties.put("replicas", data.getReplicas());
-        properties.put("repositories", data.getRepositories());
-        properties.put("serviceAccountName", data.getServiceAccountName());
-        properties.put("sources", data.getSources());
-        properties.put("template", data.getTemplate());
-        properties.put("traits", data.getTraits());
-        properties.put("flows", data.get_flows());
-        properties.values().removeAll(Collections.singleton(null));
-        jsonGenerator.writeObject(properties);
+        jsonGenerator.writeStartObject();
+
+        if (data.getConfiguration() != null) {
+            jsonGenerator.writeObjectField("configuration", data.getConfiguration());
+        }
+        if (data.getDependencies() != null) {
+            jsonGenerator.writeObjectField("dependencies", data.getDependencies());
+        }
+        if (data.getIntegrationKit() != null) {
+            jsonGenerator.writeObjectField("integrationKit", data.getIntegrationKit());
+        }
+        if (data.getProfile() != null) {
+            jsonGenerator.writeObjectField("profile", data.getProfile());
+        }
+        if (data.getReplicas() != null) {
+            jsonGenerator.writeObjectField("replicas", data.getReplicas());
+        }
+        if (data.getRepositories() != null) {
+            jsonGenerator.writeObjectField("repositories", data.getRepositories());
+        }
+        if (data.getServiceAccountName() != null) {
+            jsonGenerator.writeObjectField("serviceAccountName", data.getServiceAccountName());
+        }
+        if (data.getSources() != null) {
+            jsonGenerator.writeObjectField("sources", data.getSources());
+        }
+        if (data.getTemplate() != null) {
+            jsonGenerator.writeObjectField("template", data.getTemplate());
+        }
+        if (data.getTraits() != null) {
+            jsonGenerator.writeObjectField("traits", data.getTraits());
+        }
+        if (data.get_flows() != null) {
+            jsonGenerator.writeObjectField("flows", data.get_flows());
+        }
+        jsonGenerator.writeEndObject();
     }
 
 
